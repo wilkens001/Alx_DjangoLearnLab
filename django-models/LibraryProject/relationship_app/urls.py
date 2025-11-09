8e247a439a5bf31ac58ad5cd03f7c32738f05f70
wilkens001 <mbobwilkens@gmail.com>
2025-11-09 20:07:28 +0200

    Auto: update relationship_app urls

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from . import views
+from .views import list_books, LibraryDetailView
 
 app_name = 'relationship_app'
 
 urlpatterns = [
     # URL pattern for function-based view that lists all books
-    path('books/', views.list_books, name='list_books'),
+    path('books/', list_books, name='list_books'),
     
     # URL pattern for class-based view that shows library details
-    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 ]
