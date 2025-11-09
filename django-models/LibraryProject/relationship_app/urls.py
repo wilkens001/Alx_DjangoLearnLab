5414299a71b661e0fcb3c902ee829b237f8d3182
wilkens001 <mbobwilkens@gmail.com>
2025-11-09 19:56:45 +0200

    Update relationship_app templates, views and urls (repeat push)

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -4,9 +4,9 @@ from . import views
 app_name = 'relationship_app'
 
 urlpatterns = [
-    # Function-based view URL pattern
+    # URL pattern for function-based view that lists all books
     path('books/', views.list_books, name='list_books'),
     
-    # Class-based view URL pattern
+    # URL pattern for class-based view that shows library details
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
+from django.views.generic import ListView, DetailView
 from .models import Book, Library
 
 def list_books(request):
-    """Function-based view to list all books"""
+    """Function-based view to list all books
+    This view renders a simple text list of book titles and their authors
+    """
     books = Book.objects.all().select_related('author')
-    return render(request, 'relationship_app/list_books.html', {'books': books})
+    context = {
+        'books': books
+    }
+    return render(request, 'relationship_app/list_books.html', context)
 
 
 class LibraryDetailView(DetailView):
-    """Class-based view to display library details"""
+    """Class-based view to display details for a specific library
+    This view displays details of a specific library, listing all books available in that library
+    """
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
@@ -17,3 +24,8 @@ class LibraryDetailView(DetailView):
     def get_queryset(self):
         """Optimize query by prefetching related books and their authors"""
         return Library.objects.prefetch_related('books__author')
+
+    def get_context_data(self, **kwargs):
+        """Add additional context data"""
+        context = super().get_context_data(**kwargs)
+        return context
