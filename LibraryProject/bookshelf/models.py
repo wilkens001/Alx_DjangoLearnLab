8c60f40fb1958f2723ee7553eaa4a831d473522a
wilkens001 <mbobwilkens@gmail.com>
2025-11-09 17:58:13 +0200

    Refactor: Clean up project structure, remove unused files, and enhance documentation

--- LibraryProject/bookshelf/models.py
+++ LibraryProject/bookshelf/models.py
@@ -1,6 +1,3 @@
 from django.db import models
 
-class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
-    publication_year = models.IntegerField()
+# Create your models here.
