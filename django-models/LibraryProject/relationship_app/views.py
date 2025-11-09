ef2981394253163e626f07958a1057d05ae32af7
wilkens001 <mbobwilkens@gmail.com>
2025-11-09 20:04:26 +0200

    Auto: update relationship_app views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic.detail import DetailView
+from django.views.generic import ListView
 # Import Library model first to ensure it's available for the class-based view
 from .models import Library, Book
 
